de00b9528c55dbd39676a046a841f816297f8073
BroFrank <frankysynt@gmail.com>
2021-10-05 19:36:15 +0300

    Add state to recipe (#10)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_141610) do
+ActiveRecord::Schema.define(version: 2021_10_05_143832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_10_05_141610) do
     t.text "image_data"
     t.text "description"
     t.boolean "secret", default: false
+    t.string "state", default: "draft", null: false
   end
 
   create_table "shopping_list_items", id: :serial, force: :cascade do |t|
