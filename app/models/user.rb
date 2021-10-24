33e0a16696a79de9d24e388deb8c02ec08e0dbba
Yana Petrova <janavpetrova@gmail.com>
2021-10-24 10:45:40 +0300

    small improvements
    - 20 recipes per page at root page (better ui)
    - remove mandatory name
    - remove useless users/create view
    - fix user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :name, presence: true
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 
