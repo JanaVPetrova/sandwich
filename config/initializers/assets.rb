26bb0e43bc96dcb30a69218733664647b28a6313
Yana Petrova <janavpetrova@gmail.com>
2021-10-29 20:26:43 +0300

    fix layout & markup
    - show ingredients if ingredient group's title is nil
    - fix recipe placeholder display
    - fix bootstrap icons

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w(bootstrap-icons.woff bootstrap-icons.woff2)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
